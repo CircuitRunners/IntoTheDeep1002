e1488c3268491f5fa5e10fc70c80e7f3f41290e2
Connor Reddington <connorreddington@icloud.com>
2024-09-11 16:41:28 -0400

    Auto Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/testAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/testAuto.java
@@ -17,11 +17,14 @@ public class testAuto extends OpMode {
 
 
     private Pose startPoint = new Pose(9.757, 84.98,0);
-    private Pose pointOne = new Pose(36.784,77.275,0);
-    private Pose pointTwo = new Pose(53.608, 114.059,0);
-    private Pose pointThree = new Pose(14.828,129.743,0);
-
-
+    private Pose chamber = new Pose(36.784,77.275,0);
+    private Pose spikeMark1 = new Pose(53.608, 114.059,0);
+    private Pose net1 = new Pose(14.828,129.743,0);
+    private Pose spikeMark2 = new Pose(69.58,123.75,0);
+    private Pose net2 = new Pose(17.96,133.16,0);
+    private Pose spikeMark3 = new Pose(66.725,134.305,0);
+    private Pose net3 = new Pose(18,136.871,0);
+    private Pose observation = new Pose(18,18.250,0);
     @Override
     public void loop(){
 
