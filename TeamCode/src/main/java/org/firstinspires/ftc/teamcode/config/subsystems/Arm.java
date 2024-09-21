ededa46c3fe63317d2179f70915f81cd6e70fd41
Samarth Mahapatra <samarth.mahapatra314@gmail.com>
2024-09-20 21:55:30 -0400

    Added a new org structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Arm.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-import org.firstinspires.ftc.teamcode.config.util.RobotConstants;
+import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.config.util.HWValues;
 import org.firstinspires.ftc.teamcode.config.util.action.RunAction;
 
@@ -17,15 +17,18 @@ public class Arm {
     private Telemetry telemetry;
     public DcMotorEx arm;
     private int pos;
-    public RunAction toZero, toCollect, toLowBucket, toLowChamber, toHighChamber;
+    public RunAction toZero, toCollect, toClear, toScore, toAttach, toWinch;
 
     public PIDController armPID;
 
-    public static int target;
-    public static double p = 0, i = 0, d = 0;
+    public static double target;
+    public static double kP = 0, kI = 0, kD = 0;
     public static double f = 0.01;
+
+    public static double ARM_SPEED = 0.05;
+
     // TODO
-    private final double ticks_in_degrees = 1.5;
+    private final double ARM_TICKS_PER_DEGREE = 19.7924893140647;
 
     public Arm(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
@@ -33,26 +36,25 @@ public class Arm {
         arm = hardwareMap.get(DcMotorEx.class, HWValues.ARM);
         arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        armPID = new PIDController(p, i, d);
+        armPID = new PIDController(kP, kI, kD);
         toZero = new RunAction(this::toZero);
         toCollect = new RunAction(this::toCollect);
-        toLowBucket = new RunAction(this::toLowBucket);
-        toLowChamber = new RunAction(this::toLowChamber);
-        toHighChamber = new RunAction(this::toHighChamber);
-
-
+        toClear = new RunAction(this::toClear);
+        toScore = new RunAction(this::toScore);
+        toAttach = new RunAction(this::toAttach);
+        toWinch = new RunAction(this::toWinch);
     }
 
-    public void manual(double n) { arm.setPower(n); }
-    public void setTarget(int b) { target = b; }
+    public void manual(double n) { target += n * ARM_SPEED; }
+    public void setTarget(double b) { target = b; }
+    public double getTarget() {return target;}
 
 
     public void updatePIDF() {
-        armPID.setPID(p, i, d);
+        armPID.setPID(kP, kI, kD);
         updatePos();
         double pid = armPID.calculate(pos, target);
-        double ff = Math.cos(Math.toRadians(target/ticks_in_degrees)) * f;
+        double ff = Math.cos(Math.toRadians(target/ ARM_TICKS_PER_DEGREE)) * f;
         double power = pid + ff;
 
         arm.setPower(power);
@@ -66,23 +68,28 @@ public class Arm {
     }
     public void toCollect() {
         updatePos();
-        setTarget(0);
-    }
-    public void toLowBucket() {
-        updatePos();
-        setTarget(0);
+        setTarget(250 * ARM_TICKS_PER_DEGREE);
     }
 
-    public void toLowChamber() {
+    public void toClear() {
         updatePos();
-        setTarget(0);
+        setTarget(230 * ARM_TICKS_PER_DEGREE);
     }
 
-    public void toHighChamber() {
+    public void toScore() {
         updatePos();
-        setTarget(0);
+        setTarget(160 * ARM_TICKS_PER_DEGREE);
     }
 
+    public void toAttach() {
+        updatePos();
+        setTarget(120 * ARM_TICKS_PER_DEGREE);
+    }
+
+    public void toWinch() {
+        updatePos();
+        setTarget(15 * ARM_TICKS_PER_DEGREE);
+    }
 
     // Util //
 
