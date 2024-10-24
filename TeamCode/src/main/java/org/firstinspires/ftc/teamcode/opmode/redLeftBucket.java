4fb16995fc28a8b61f79d79e5d307e91c64fe00a
Lovetocode141 <Christian.heuser@circuitrunners.com>
2024-10-24 17:59:18 -0400

    Red auto in test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/redLeftBucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/redLeftBucket.java
@@ -9,12 +9,17 @@ import org.firstinspires.ftc.teamcode.config.pedroPathing.pathGeneration.BezierL
 import org.firstinspires.ftc.teamcode.config.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.config.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.config.pedroPathing.util.Timer;
-import org.firstinspires.ftc.teamcode.config.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.config.subsystems.EndEffector;
+import org.firstinspires.ftc.teamcode.config.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.config.util.action.Action;
 import org.firstinspires.ftc.teamcode.config.util.action.Actions;
+import org.firstinspires.ftc.teamcode.config.util.action.ParallelAction;
+import org.firstinspires.ftc.teamcode.config.util.action.SequentialAction;
+import org.firstinspires.ftc.teamcode.config.util.action.SleepAction;
+
 
 @Autonomous
-public class redLeftBucket extends OpMode {
+public class redLeftBucket extends OpMode{
     private Follower follower;
     private Timer pathTimer;
     private int pathState;
@@ -23,14 +28,15 @@ public class redLeftBucket extends OpMode {
     private EndEffector endEffector;
 
     // Define key poses
-    private Pose startPosition = new Pose(133.5, 60, Math.toRadians(180));
-    private Pose bucketClear = new Pose(125, 18, Math.toRadians(315));
-    private Pose bucketPos = new Pose(133, 15,Math.toRadians(315));
-    private Pose parkPos = new Pose(133.5, 110, Math.toRadians(180));
-    //points are still untested/rough
+    private Pose startPosition = new Pose(132.5, 60, Math.toRadians(0));
+    private Pose bucketClear = new Pose(120, 30, Math.toRadians(135));
+    private Pose bucketPos = new Pose(133.8, 15,Math.toRadians(135));
+    private Pose preprePark = new Pose(133,35,Math.toRadians(180));
+    private Pose prePark = new Pose(92,35,Math.toRadians(90));
+    private Pose parkPos = new Pose(85, 38, Math.toRadians(90));
 
 
-    private PathChain basketClear, score;
+    private PathChain basketClear, score, park;
     public void buildPaths() {
         basketClear = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPosition), new Point(bucketClear)))
@@ -40,6 +46,14 @@ public class redLeftBucket extends OpMode {
                 .addPath(new BezierLine(new Point(bucketClear), new Point(bucketPos)))
                 .setConstantHeadingInterpolation(bucketPos.getHeading())
                 .build();
+        park = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(bucketPos), new Point(preprePark)))
+                .setConstantHeadingInterpolation(preprePark.getHeading())
+                .addPath(new BezierLine(new Point(preprePark), new Point(prePark)))
+                .setConstantHeadingInterpolation(prePark.getHeading())
+                .addPath(new BezierLine(new Point(prePark), new Point(parkPos)))
+                .setConstantHeadingInterpolation(parkPos.getHeading())
+                .build();
     }
 
     public void autonomousPathUpdate() {
@@ -48,24 +62,50 @@ public class redLeftBucket extends OpMode {
                 Actions.runBlocking(endEffector.closeClaw);
                 Actions.runBlocking(endEffector.diffyIdle);
                 setPathState(1);
+                break;
             case 1:
-                Actions.runBlocking(endEffector.closeClaw);
-                follower.followPath(basketClear);
-                setPathState(2);
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(endEffector.closeClaw);
+                    follower.followPath(basketClear);
+                    setPathState(2);
+                }
                 break;
             case 2:
-                Actions.runBlocking(endEffector.closeClaw);
-                Actions.runBlocking(arm.armLowBasket);
-                Actions.runBlocking(endEffector.diffyBasket);
-                setPathState(3);
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(endEffector.closeClaw);
+                    Actions.runBlocking(arm.armLowBasket);
+                    Actions.runBlocking(endEffector.diffyBasket);
+//                    Actions.runBlocking(new SleepAction(1));
+                    setPathState(3);
+                }
+                break;
             case 3:
-                Actions.runBlocking(endEffector.closeClaw);
-                follower.followPath(score);
-                setPathState(4);
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(endEffector.closeClaw);
+                    follower.followPath(score);
+                    setPathState(4);
+                }
+                break;
             case 4:
-                //Actions.runBlocking(endEffector.openClaw);
-                setPathState(5);
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(endEffector.openClaw);
+                    setPathState(5);
+                }
+                break;
             case 5:
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(resetArm());
+                    follower.followPath(park);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                if (!follower.isBusy()) {
+                    Actions.runBlocking(arm.armSpecimenScore);
+                    Actions.runBlocking(new SleepAction(5));
+                    setPathState(7);
+                }
+            case 7:
                 if (!follower.isBusy()) {
                     setPathState(-1);
                 }
@@ -102,7 +142,8 @@ public class redLeftBucket extends OpMode {
         follower.setStartingPose(startPosition);
         arm = new Arm(hardwareMap);
         endEffector = new EndEffector(hardwareMap);
-        //endEffector.closeClaw();
+        endEffector.closeClaw();
+        endEffector.idlePosition();
         buildPaths();
     }
 
@@ -110,6 +151,17 @@ public class redLeftBucket extends OpMode {
     public void start() {
         pathTimer.resetTimer();
         setPathState(0);
+    }
+
+    public Action resetArm() {
+        return new SequentialAction(
+                new SleepAction(1),
+                new ParallelAction(
+                        endEffector.diffyIdle,
+                        arm.armObservation
+                ),
+                arm.armUpright
+        );
     }
 
 }
